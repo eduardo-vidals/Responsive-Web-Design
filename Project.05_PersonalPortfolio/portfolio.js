3cde46c9e328c78346542cc25bcff1a19a2aa6b1
Eduardo Vidals <63515838+eduardo-vidals@users.noreply.github.com>
2021-05-30 21:25:30 -0400

    Personal portfolio finished

---
+++ Project.05_PersonalPortfolio/portfolio.js
@@ -0,0 +1,9 @@
+function copyElementText(id) {
+    const text = document.getElementById(id).innerText;
+    const elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+}
